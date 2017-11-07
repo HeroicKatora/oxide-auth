970cc7e7fe8ad12cb2dff7728f7cb137e53dce48
HeroicKatora <andreas.molzer@gmx.de>
2017-11-07 07:34:25 +0100

    Documentation for backend

--- src/code_grant/backend.rs
+++ src/code_grant/backend.rs
@@ -1,3 +1,14 @@
+//! Available backend algorithsm.
+//!
+//! The backend codifies the requirements from the rfc into types and functions as safely as
+//! possible. It is, in contrast to the frontend, not concrete in the required type but rather
+//! uses a very generic internal reqpresentation.
+//! The result of the backend are abstract results, actions which should be executed or relayed
+//! by the frontend usis its available types. Abstract in this sense means that the reponsens
+//! from the backend are not generic on an input type.
+//! Another consideration is the possiblilty of reusing some components with other oauth schemes.
+//! In this way, the backend is used to group necessary types and as an interface to implementors,
+//! to be able to infer the range of applicable end effectors (i.e. authorizers, issuer, registrars).
 use super::{Authorizer, ClientParameter, QueryMap, Registrar, RegistrarError};
 use super::{Negotiated, NegotiationParameter};
 use super::{Issuer, IssuedToken, Request};
@@ -29,6 +40,7 @@ pub fn decode_query<'u>(kvpairs: QueryMap<'u>) -> Result<ClientParameter<'u>, St
     })
 }
 
+/// CodeRef is a thin wrapper around necessary types to execute an authorization code grant.
 pub struct CodeRef<'a> {
     registrar: &'a Registrar,
     authorizer: &'a mut Authorizer,
@@ -40,6 +52,15 @@ pub enum CodeError {
 }
 
 impl<'u> CodeRef<'u> {
+    /// Retrieve allowed scope and redirect url from the registrar.
+    ///
+    /// Checks the validity of any given input as the registrar instance communicates the registrated
+    /// parameters. The registrar can also set or override the requested (default) scope of the client.
+    /// This will result in a tuple of negotiated parameters which can be used further to authorize
+    /// the client by the owner or, in case of errors, in an action to be taken.
+    /// If the client is not registered, the request will otherwise be ignored, if the request has
+    /// some other syntactical error, the client is contacted at its redirect url with an error
+    /// response.
     pub fn negotiate<'a>(&self, client_id: Cow<'a, str>, scope: Option<Cow<'a, str>>, redirect_url: Option<Cow<'a, Url>>)
     -> Result<Negotiated<'a>, CodeError> {
         let result = match self.registrar.negotiate(NegotiationParameter{client_id, scope, redirect_url}) {
@@ -51,6 +72,7 @@ impl<'u> CodeRef<'u> {
         Ok(result)
     }
 
+    /// Use negotiated parameters to authorize a client for an owner.
     pub fn authorize<'a>(&'a mut self, owner_id: Cow<'a, str>, negotiated: Negotiated<'a>, state: Option<Cow<'a, str>>)
      -> Result<Url, CodeError> {
         let grant = self.authorizer.authorize(Request{
@@ -71,12 +93,14 @@ impl<'u> CodeRef<'u> {
     }
 }
 
+/// Issuer is a thin wrapper around necessary types to execute an bearer token request..
 pub struct IssuerRef<'a> {
     authorizer: &'a mut Authorizer,
     issuer: &'a mut Issuer,
 }
 
 impl<'u> IssuerRef<'u> {
+    /// Try to redeem an authorization code.
     pub fn use_code<'a>(&'a mut self, code: String, expected_client: Cow<'a, str>, expected_url: Cow<'a, str>)
     -> Result<IssuedToken, Cow<'static, str>> {
         let saved_params = match self.authorizer.recover_parameters(code.as_ref()) {
