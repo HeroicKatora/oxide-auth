234cd819739aeab9d6762c4ca712f386876d96b2
Andreas Molzer <andreas.molzer@gmx.de>
2018-04-12 00:43:14 +0200

    Add deferred authorizer
    This type caches an authorization request.  This makes it possible for
    the core library to try authorization with the deferred authorizer
    mutiple times, which will fail until the request has asynchronously
    resolved.  This failure can be caught and handled by the actix layer.
    An actual request is only be sent exactly once with this mechanism.

--- src/frontends/actix/primitives.rs
+++ src/frontends/actix/primitives.rs
@@ -2,21 +2,21 @@
 use super::defer::DeferableComputation;
 use super::actix::dev::*;
 
-use std::marker::PhantomData;
+use super::futures::Future;
 
 use primitives::prelude::*;
 use primitives::grant::Grant;
 
 pub struct AuthorizeActor<A: Authorizer>(A);
 
+impl<A: Authorizer + 'static> Actor for AuthorizeActor<A> {
+    type Context = Context<Self>;
+}
+
 pub struct AuthorizationRequest(pub Grant);
 
 impl Message for AuthorizationRequest {
     type Result = Result<String, ()>;
-}
-
-impl<A: Authorizer + 'static> Actor for AuthorizeActor<A> {
-    type Context = Context<Self>;
 }
 
 impl<A: Authorizer + 'static> Handler<AuthorizationRequest> for AuthorizeActor<A> {
@@ -38,5 +38,22 @@ impl<A: Authorizer + 'static> Handler<ExtractAuthorization> for AuthorizeActor<A
 
     fn handle(&mut self, msg: ExtractAuthorization, _: &mut Self::Context) -> Self::Result {
         MessageResult(self.0.extract(&msg.0))
+    }
+}
+
+pub struct DeferredAuthorizer<A: Authorizer + 'static> {
+    connection: Addr<Unsync, AuthorizeActor<A>>,
+    cached_result: DeferableComputation<Request<Unsync, AuthorizeActor<A>, AuthorizationRequest>>,
+}
+
+impl<A: Authorizer + 'static> DeferredAuthorizer<A> {
+    fn authorize(&mut self, grant: Grant) -> Result<String, ()> {
+        let connection = self.connection.clone();
+        let retrieve = move || connection.send(AuthorizationRequest(grant));
+        self.cached_result.initialize(retrieve);
+        match self.cached_result.make_answer() {
+            Some(Ok(token)) => Ok(token),
+            _ => Err(())
+        }
     }
 }
