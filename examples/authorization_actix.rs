558b1b925c35bb35fea6c7164d01bef0b3bc2bbd
Andreas Molzer <andreas.molzer@gmx.de>
2018-05-30 21:01:54 +0200

    Cleanup example structure
    The feature guards are now provided as top level attributes.  This means
    they will fail to compile when requested with missing features.

--- examples/authorization_actix.rs
+++ examples/authorization_actix.rs
@@ -1,160 +1,154 @@
- mod support;
+#![cfg(feature = "actix-frontend")]
 
-#[cfg(feature = "actix-frontend")]
-mod main {
-    extern crate actix;
-    extern crate actix_web;
-    extern crate futures;
-    extern crate oxide_auth;
-    extern crate url;
+mod support;
+extern crate actix;
+extern crate actix_web;
+extern crate futures;
+extern crate oxide_auth;
+extern crate url;
 
-    use super::support::actix::dummy_client;
-    use self::actix::{Actor, Addr, Syn};
-    use self::actix_web::{server, App, Body, HttpRequest, HttpResponse, Error as AWError};
-    use self::actix_web::http::Method;
-    use self::futures::Future;
-    use self::oxide_auth::frontends::actix::*;
-    use self::oxide_auth::code_grant::frontend::{OAuthError, OwnerAuthorization};
-    use self::oxide_auth::primitives::prelude::*;
+use actix::{Actor, Addr, Syn};
+use actix_web::{server, App, Body, HttpRequest, HttpResponse, Error as AWError};
+use actix_web::http::Method;
+use futures::Future;
 
-    static PASSPHRASE: &str = "This is a super secret phrase";
-    static DENY_TEXT: &str = "<html>
+use oxide_auth::frontends::actix::*;
+use oxide_auth::code_grant::frontend::{OAuthError, OwnerAuthorization};
+use oxide_auth::primitives::prelude::*;
+use support::actix::dummy_client;
+use support::open_in_browser;
+
+static PASSPHRASE: &str = "This is a super secret phrase";
+static DENY_TEXT: &str = "<html>
 This page should be accessed via an oauth token from the client in the example. Click
 <a href=\"http://localhost:8020/authorize?response_type=code&client_id=LocalClient\">
 here</a> to begin the authorization process.
 </html>
 ";
 
-    /// Example of a main function of a rouille server supporting oauth.
-    pub fn example() {
-        let sys = actix::System::new("HttpServer");
+/// Example of a main function of a rouille server supporting oauth.
+pub fn main() {
+    let sys = actix::System::new("HttpServer");
 
-        let mut clients  = ClientMap::new();
-        // Register a dummy client instance
-        let client = Client::public("LocalClient", // Client id
-            "http://localhost:8021/endpoint".parse().unwrap(), // Redirection url
-            "default".parse().unwrap()); // Allowed client scope
-        clients.register_client(client);
+    let mut clients  = ClientMap::new();
+    // Register a dummy client instance
+    let client = Client::public("LocalClient", // Client id
+        "http://localhost:8021/endpoint".parse().unwrap(), // Redirection url
+        "default".parse().unwrap()); // Allowed client scope
+    clients.register_client(client);
 
-        let authorizer = Storage::new(RandomGenerator::new(16));
-        let issuer = TokenSigner::new_from_passphrase(&PASSPHRASE, None);
-        let scopes = vec!["default".parse().unwrap()].into_boxed_slice();
+    let authorizer = Storage::new(RandomGenerator::new(16));
+    let issuer = TokenSigner::new_from_passphrase(&PASSPHRASE, None);
+    let scopes = vec!["default".parse().unwrap()].into_boxed_slice();
 
-        // Emulate static initialization for complex type
-        let scopes: &'static _ = Box::leak(scopes);
+    // Emulate static initialization for complex type
+    let scopes: &'static _ = Box::leak(scopes);
 
-        let endpoint: Addr<Syn,_> = CodeGrantEndpoint::new((clients, authorizer, issuer))
-            .with_authorization(|&mut (ref client, ref mut authorizer, _)| {
-                AuthorizationFlow::new(client, authorizer)
+    let endpoint: Addr<Syn,_> = CodeGrantEndpoint::new((clients, authorizer, issuer))
+        .with_authorization(|&mut (ref client, ref mut authorizer, _)| {
+            AuthorizationFlow::new(client, authorizer)
+        })
+        .with_grant(|&mut (ref client, ref mut authorizer, ref mut issuer)| {
+            GrantFlow::new(client, authorizer, issuer)
+        })
+        .with_guard(move |&mut (_, _, ref mut issuer)| {
+            AccessFlow::new(issuer, scopes)
+        })
+        .start();
+
+    // Create the main server instance
+    server::new(
+        move || App::with_state(endpoint.clone())
+            .route("/authorize", Method::GET, |req: HttpRequest<_>| {
+                let endpoint = req.state().clone();
+                Box::new(req.oauth2()
+                    .authorization_code(handle_get)
+                    .and_then(move |request| endpoint.send(request)
+                        .or_else(|_| Err(OAuthError::AccessDenied))
+                        .and_then(|result| result.and_then(ResolvedResponse::into))
+                    )
+                    .or_else(|_| Ok(HttpResponse::BadRequest().body(Body::Empty)))
+                ) as Box<Future<Item = HttpResponse, Error = AWError>>
             })
-            .with_grant(|&mut (ref client, ref mut authorizer, ref mut issuer)| {
-                GrantFlow::new(client, authorizer, issuer)
+            .route("/authorize", Method::POST, |req: HttpRequest<_>| {
+                let endpoint = req.state().clone();
+                let denied = req.query_string().contains("deny");
+                Box::new(req.oauth2()
+                    .authorization_code(move |grant| handle_post(denied, grant))
+                    .and_then(move |request| endpoint.send(request)
+                        .or_else(|_| Err(OAuthError::AccessDenied))
+                        .and_then(|result| result.and_then(ResolvedResponse::into))
+                    )
+                    .or_else(|_| Ok(HttpResponse::BadRequest().body(Body::Empty)))
+                ) as Box<Future<Item = HttpResponse, Error = AWError>>
             })
-            .with_guard(move |&mut (_, _, ref mut issuer)| {
-                AccessFlow::new(issuer, scopes)
+            .route("/token", Method::POST, |req: HttpRequest<_>| {
+                let endpoint = req.state().clone();
+                Box::new(req.oauth2()
+                    .access_token()
+                    .and_then(move |request| endpoint.send(request)
+                        .or_else(|_| Err(OAuthError::AccessDenied))
+                        .and_then(|result| result.and_then(ResolvedResponse::into))
+                    )
+                    .or_else(|_| Ok(HttpResponse::BadRequest().body(Body::Empty)))
+                ) as Box<Future<Item = HttpResponse, Error = AWError>>
             })
-            .start();
+            .route("/", Method::GET, |req: HttpRequest<_>| {
+                let endpoint = req.state().clone();
+                Box::new(req.oauth2()
+                    .guard()
+                    .and_then(move |request| endpoint.send(request)
+                        .or_else(|_| Err(OAuthError::AccessDenied))
+                        .and_then(|result| result)
+                    ).map(|()|
+                        HttpResponse::Ok()
+                            .content_type("text/plain")
+                            .body("Hello world!")
+                    ).or_else(|_|
+                        Ok(HttpResponse::Unauthorized()
+                            .content_type("text/html")
+                            .body(DENY_TEXT))
+                    )
+                ) as Box<Future<Item = HttpResponse, Error = AWError>>
+            })
+        )
+        .bind("localhost:8020")
+        .expect("Failed to bind to socket")
+        .start();
 
-        // Create the main server instance
-        server::new(
-            move || App::with_state(endpoint.clone())
-                .route("/authorize", Method::GET, |req: HttpRequest<_>| {
-                    let endpoint = req.state().clone();
-                    Box::new(req.oauth2()
-                        .authorization_code(handle_get)
-                        .and_then(move |request| endpoint.send(request)
-                            .or_else(|_| Err(OAuthError::AccessDenied))
-                            .and_then(|result| result.and_then(ResolvedResponse::into))
-                        )
-                        .or_else(|_| Ok(HttpResponse::BadRequest().body(Body::Empty)))
-                    ) as Box<Future<Item = HttpResponse, Error = AWError>>
-                })
-                .route("/authorize", Method::POST, |req: HttpRequest<_>| {
-                    let endpoint = req.state().clone();
-                    let denied = req.query_string().contains("deny");
-                    Box::new(req.oauth2()
-                        .authorization_code(move |grant| handle_post(denied, grant))
-                        .and_then(move |request| endpoint.send(request)
-                            .or_else(|_| Err(OAuthError::AccessDenied))
-                            .and_then(|result| result.and_then(ResolvedResponse::into))
-                        )
-                        .or_else(|_| Ok(HttpResponse::BadRequest().body(Body::Empty)))
-                    ) as Box<Future<Item = HttpResponse, Error = AWError>>
-                })
-                .route("/token", Method::POST, |req: HttpRequest<_>| {
-                    let endpoint = req.state().clone();
-                    Box::new(req.oauth2()
-                        .access_token()
-                        .and_then(move |request| endpoint.send(request)
-                            .or_else(|_| Err(OAuthError::AccessDenied))
-                            .and_then(|result| result.and_then(ResolvedResponse::into))
-                        )
-                        .or_else(|_| Ok(HttpResponse::BadRequest().body(Body::Empty)))
-                    ) as Box<Future<Item = HttpResponse, Error = AWError>>
-                })
-                .route("/", Method::GET, |req: HttpRequest<_>| {
-                    let endpoint = req.state().clone();
-                    Box::new(req.oauth2()
-                        .guard()
-                        .and_then(move |request| endpoint.send(request)
-                            .or_else(|_| Err(OAuthError::AccessDenied))
-                            .and_then(|result| result)
-                        ).map(|()|
-                            HttpResponse::Ok()
-                                .content_type("text/plain")
-                                .body("Hello world!")
-                        ).or_else(|_|
-                            Ok(HttpResponse::Unauthorized()
-                                .content_type("text/html")
-                                .body(DENY_TEXT))
-                        )
-                    ) as Box<Future<Item = HttpResponse, Error = AWError>>
-                })
-            )
-            .bind("localhost:8020")
-            .expect("Failed to bind to socket")
-            .start();
+    server::new(|| App::new().handler("/endpoint", dummy_client))
+        .bind("localhost:8021")
+        .expect("Failed to start dummy client")
+        .start();
 
-        server::new(|| App::new().handler("/endpoint", dummy_client))
-            .bind("localhost:8021")
-            .expect("Failed to start dummy client")
-            .start();
-
-        let _ = sys.run();
-    }
-
-    /// A simple implementation of the first part of an authentication handler. This will
-    /// display a page to the user asking for his permission to proceed. The submitted form
-    /// will then trigger the other authorization handler which actually completes the flow.
-    fn handle_get(grant: &PreGrant) -> OwnerAuthorization<ResolvedResponse> {
-        let text = format!(
-            "<html>'{}' (at {}) is requesting permission for '{}'
-            <form action=\"authorize?response_type=code&client_id={}\" method=\"post\">
-                <input type=\"submit\" value=\"Accept\">
-            </form>
-            <form action=\"authorize?response_type=code&client_id={}&deny=1\" method=\"post\">
-                <input type=\"submit\" value=\"Deny\">
-            </form>
-            </html>", grant.client_id, grant.redirect_uri, grant.scope, grant.client_id, grant.client_id);
-        let response = ResolvedResponse::html(&text);
-        OwnerAuthorization::InProgress(response)
-    }
-
-    /// Handle form submission by a user, completing the authorization flow. The resource owner
-    /// either accepted or denied the request.
-    fn handle_post(denied: bool, _: &PreGrant) -> OwnerAuthorization<ResolvedResponse> {
-        // No real user authentication is done here, in production you SHOULD use session keys or equivalent
-        if denied {
-            OwnerAuthorization::Denied
-        } else {
-            OwnerAuthorization::Authorized("dummy user".to_string())
-        }
-    }
+    sys.handle().spawn_fn(|| Ok(open_in_browser()));
+    let _ = sys.run();
 }
 
-#[cfg(not(feature = "actix-frontend"))]
-mod main { pub fn example() { } }
+/// A simple implementation of the first part of an authentication handler. This will
+/// display a page to the user asking for his permission to proceed. The submitted form
+/// will then trigger the other authorization handler which actually completes the flow.
+fn handle_get(grant: &PreGrant) -> OwnerAuthorization<ResolvedResponse> {
+    let text = format!(
+        "<html>'{}' (at {}) is requesting permission for '{}'
+        <form action=\"authorize?response_type=code&client_id={}\" method=\"post\">
+            <input type=\"submit\" value=\"Accept\">
+        </form>
+        <form action=\"authorize?response_type=code&client_id={}&deny=1\" method=\"post\">
+            <input type=\"submit\" value=\"Deny\">
+        </form>
+        </html>", grant.client_id, grant.redirect_uri, grant.scope, grant.client_id, grant.client_id);
+    let response = ResolvedResponse::html(&text);
+    OwnerAuthorization::InProgress(response)
+}
 
-fn main() {
-    main::example();
+/// Handle form submission by a user, completing the authorization flow. The resource owner
+/// either accepted or denied the request.
+fn handle_post(denied: bool, _: &PreGrant) -> OwnerAuthorization<ResolvedResponse> {
+    // No real user authentication is done here, in production you SHOULD use session keys or equivalent
+    if denied {
+        OwnerAuthorization::Denied
+    } else {
+        OwnerAuthorization::Authorized("dummy user".to_string())
+    }
 }
--- examples/authorization_rouille.rs
+++ examples/authorization_rouille.rs
@@ -1,137 +1,126 @@
+#![cfg(feature = "rouille-frontend")]
 mod support;
 
-#[cfg(feature = "rouille-frontend")]
 #[macro_use]
 extern crate rouille;
+extern crate oxide_auth;
+extern crate url;
 
-#[cfg(feature = "rouille-frontend")]
-mod main {
-    extern crate oxide_auth;
-    extern crate url;
+use rouille::{Request, Response, Server};
+use oxide_auth::frontends::rouille::*;
 
-    use super::rouille::{Request, Response, Server};
-    use self::oxide_auth::frontends::rouille::*;
+use support::rouille::dummy_client;
+use support::open_in_browser;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-    use support::rouille::dummy_client;
-    use support::open_in_browser;
-    use std::sync::{Arc, Mutex};
-    use std::thread;
+/// Example of a main function of a rouille server supporting oauth.
+pub fn main() {
+    // Stores clients in a simple in-memory hash map.
+    let clients =  {
+        let mut clients = ClientMap::new();
+        // Register a dummy client instance
+        let client = Client::public("LocalClient", // Client id
+            "http://localhost:8021/endpoint".parse().unwrap(), // Redirection url
+            "default".parse().unwrap()); // Allowed client scope
+        clients.register_client(client);
+        Mutex::new(clients)
+    };
 
-    /// Example of a main function of a rouille server supporting oauth.
-    pub fn example() {
-        // Stores clients in a simple in-memory hash map.
-        let clients =  {
-            let mut clients = ClientMap::new();
-            // Register a dummy client instance
-            let client = Client::public("LocalClient", // Client id
-                "http://localhost:8021/endpoint".parse().unwrap(), // Redirection url
-                "default".parse().unwrap()); // Allowed client scope
-            clients.register_client(client);
-            Mutex::new(clients)
-        };
+    // Authorization tokens are 16 byte random keys to a memory hash map.
+    let authorization_codes = Mutex::new(Storage::new(RandomGenerator::new(16)));
 
-        // Authorization tokens are 16 byte random keys to a memory hash map.
-        let authorization_codes = Mutex::new(Storage::new(RandomGenerator::new(16)));
+    // Bearer tokens are signed (but not encrypted) using a passphrase.
+    let passphrase = "This is a super secret phrase";
+    let bearer_tokens = Arc::new(TokenSigner::new_from_passphrase(passphrase, None));
 
-        // Bearer tokens are signed (but not encrypted) using a passphrase.
-        let passphrase = "This is a super secret phrase";
-        let bearer_tokens = Arc::new(TokenSigner::new_from_passphrase(passphrase, None));
-
-        // Create the main server instance
-        let server = Server::new(("localhost", 8020), move |request| {
-            router!(request,
-                (GET) ["/"] => {
-                    let mut issuer = &*bearer_tokens;
-                    if let Err(_) = AccessFlow::new(&mut issuer, &vec!["default".parse().unwrap()])
-                        .handle(request)
-                    { // Does not have the proper authorization token
+    // Create the main server instance
+    let server = Server::new(("localhost", 8020), move |request| {
+        router!(request,
+            (GET) ["/"] => {
+                let mut issuer = &*bearer_tokens;
+                if let Err(_) = AccessFlow::new(&mut issuer, &vec!["default".parse().unwrap()])
+                    .handle(request)
+                { // Does not have the proper authorization token
 let text = "<html>
 This page should be accessed via an oauth token from the client in the example. Click
 <a href=\"http://localhost:8020/authorize?response_type=code&client_id=LocalClient\">
 here</a> to begin the authorization process.
 </html>
 ";
-                        Response::html(text)
-                    } else { // Allowed to access!
-                        Response::text("Hello world!")
-                    }
-                },
-                (GET) ["/authorize"] => {
-                    let mut registrar = clients.lock().unwrap();
-                    let mut authorizer = authorization_codes.lock().unwrap();
-                    AuthorizationFlow::new(&mut*registrar, &mut*authorizer)
-                        .handle(request).complete(&handle_get)
-                        .unwrap_or_else(|_| Response::empty_400())
-                },
-                (POST) ["/authorize"] => {
-                    let mut registrar = clients.lock().unwrap();
-                    let mut authorizer = authorization_codes.lock().unwrap();
-                    AuthorizationFlow::new(&mut*registrar, &mut*authorizer)
-                        .handle(request).complete(&handle_post)
-                        .unwrap_or_else(|_| Response::empty_400())
-                },
-                (POST) ["/token"] => {
-                    let mut authorizer = authorization_codes.lock().unwrap();
-                    let mut issuer = &*bearer_tokens;
-                    let mut registrar = clients.lock().unwrap();
-                    GrantFlow::new(&mut*registrar, &mut*authorizer, &mut issuer)
-                        .handle(request)
-                        .unwrap_or_else(|_| Response::empty_400())
-                },
-                _ => Response::empty_404()
-            )
-        });
+                    Response::html(text)
+                } else { // Allowed to access!
+                    Response::text("Hello world!")
+                }
+            },
+            (GET) ["/authorize"] => {
+                let mut registrar = clients.lock().unwrap();
+                let mut authorizer = authorization_codes.lock().unwrap();
+                AuthorizationFlow::new(&mut*registrar, &mut*authorizer)
+                    .handle(request).complete(&handle_get)
+                    .unwrap_or_else(|_| Response::empty_400())
+            },
+            (POST) ["/authorize"] => {
+                let mut registrar = clients.lock().unwrap();
+                let mut authorizer = authorization_codes.lock().unwrap();
+                AuthorizationFlow::new(&mut*registrar, &mut*authorizer)
+                    .handle(request).complete(&handle_post)
+                    .unwrap_or_else(|_| Response::empty_400())
+            },
+            (POST) ["/token"] => {
+                let mut authorizer = authorization_codes.lock().unwrap();
+                let mut issuer = &*bearer_tokens;
+                let mut registrar = clients.lock().unwrap();
+                GrantFlow::new(&mut*registrar, &mut*authorizer, &mut issuer)
+                    .handle(request)
+                    .unwrap_or_else(|_| Response::empty_400())
+            },
+            _ => Response::empty_404()
+        )
+    });
 
-        // Run the server main loop in another thread
-        let join = thread::spawn(move ||
-            server.expect("Failed to start server")
-                .run()
-        );
-        // Start a dummy client instance which simply relays the token/response
-        let client = thread::spawn(||
-            Server::new(("localhost", 8021), dummy_client)
-                .expect("Failed to start client")
-                .run()
-        );
+    // Run the server main loop in another thread
+    let join = thread::spawn(move ||
+        server.expect("Failed to start server")
+            .run()
+    );
+    // Start a dummy client instance which simply relays the token/response
+    let client = thread::spawn(||
+        Server::new(("localhost", 8021), dummy_client)
+            .expect("Failed to start client")
+            .run()
+    );
 
-        // Try to direct the browser to an url initiating the flow
-        open_in_browser();
-        join.join().expect("Failed to run");
-        client.join().expect("Failed to run client");
-    }
-
-    /// A simple implementation of the first part of an authentication handler. This will
-    /// display a page to the user asking for his permission to proceed. The submitted form
-    /// will then trigger the other authorization handler which actually completes the flow.
-    fn handle_get(_: &Request, grant: &PreGrant) -> OwnerAuthorization<Response> {
-        let text = format!(
-            "<html>'{}' (at {}) is requesting permission for '{}'
-            <form action=\"authorize?response_type=code&client_id={}\" method=\"post\">
-                <input type=\"submit\" value=\"Accept\">
-            </form>
-            <form action=\"authorize?response_type=code&client_id={}&deny=1\" method=\"post\">
-                <input type=\"submit\" value=\"Deny\">
-            </form>
-            </html>", grant.client_id, grant.redirect_uri, grant.scope, grant.client_id, grant.client_id);
-        let response = Response::html(text);
-        OwnerAuthorization::InProgress(response)
-    }
-
-    /// Handle form submission by a user, completing the authorization flow. The resource owner
-    /// either accepted or denied the request.
-    fn handle_post(request: &Request, _: &PreGrant) -> OwnerAuthorization<Response> {
-        // No real user authentication is done here, in production you SHOULD use session keys or equivalent
-        if let Some(_) = request.get_param("deny") {
-            OwnerAuthorization::Denied
-        } else {
-            OwnerAuthorization::Authorized("dummy user".to_string())
-        }
-    }
+    // Try to direct the browser to an url initiating the flow
+    open_in_browser();
+    join.join().expect("Failed to run");
+    client.join().expect("Failed to run client");
 }
 
-#[cfg(not(feature = "iron-frontend"))]
-mod main { pub fn example() { } }
+/// A simple implementation of the first part of an authentication handler. This will
+/// display a page to the user asking for his permission to proceed. The submitted form
+/// will then trigger the other authorization handler which actually completes the flow.
+fn handle_get(_: &Request, grant: &PreGrant) -> OwnerAuthorization<Response> {
+    let text = format!(
+        "<html>'{}' (at {}) is requesting permission for '{}'
+        <form action=\"authorize?response_type=code&client_id={}\" method=\"post\">
+            <input type=\"submit\" value=\"Accept\">
+        </form>
+        <form action=\"authorize?response_type=code&client_id={}&deny=1\" method=\"post\">
+            <input type=\"submit\" value=\"Deny\">
+        </form>
+        </html>", grant.client_id, grant.redirect_uri, grant.scope, grant.client_id, grant.client_id);
+    let response = Response::html(text);
+    OwnerAuthorization::InProgress(response)
+}
 
-fn main() {
-    main::example();
+/// Handle form submission by a user, completing the authorization flow. The resource owner
+/// either accepted or denied the request.
+fn handle_post(request: &Request, _: &PreGrant) -> OwnerAuthorization<Response> {
+    // No real user authentication is done here, in production you SHOULD use session keys or equivalent
+    if let Some(_) = request.get_param("deny") {
+        OwnerAuthorization::Denied
+    } else {
+        OwnerAuthorization::Authorized("dummy user".to_string())
+    }
 }
