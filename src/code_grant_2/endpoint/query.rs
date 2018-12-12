31dd0f5f7b8570722d10a7a5ba494b43b43056df
HeroicKatora <andreas.molzer@gmx.de>
2018-12-12 23:01:05 +0100

    Implement many QueryParameter types

--- src/code_grant_2/endpoint/query.rs
+++ src/code_grant_2/endpoint/query.rs
@@ -1,5 +1,9 @@
 use std::borrow::{Borrow, Cow};
 use std::collections::HashMap;
+use std::hash::Hash;
+use std::ops::Deref;
+use std::rc::Rc;
+use std::sync::Arc;
 
 /// Allows access to the query parameters in an url or a body.
 ///
@@ -63,18 +67,121 @@ impl ToOwned for QueryParameter {
     }
 }
 
-unsafe impl QueryParameter for HashMap<String, String> {
+/// Return a reference to value in a collection if it is the only one.
+///
+/// For example, a vector of string like types returns a reference to its first
+/// element if there are no other, else it returns `None`.
+///
+/// If this were done with slices, that would require choosing a particular
+/// value type of the underlying slice e.g. `[String]`.
+pub unsafe trait UniqueValue {
+    /// Borrow the unique value reference.
+    fn get_unique(&self) -> Option<&str>;
+}
+
+unsafe impl<K, V> QueryParameter for HashMap<K, V>
+where
+    K: Borrow<str> + Eq + Hash,
+    V: UniqueValue + Eq + Hash,
+{
     fn unique_value(&self, key: &str) -> Option<Cow<str>> {
-        self.get(key).cloned().map(Cow::Owned)
+        self.get(key).and_then(V::get_unique).map(Cow::Borrowed)
     }
 
     fn normalize(&self) -> NormalizedParameter {
-        let inner = self.iter()
-            .map(|(key, val)| (Cow::Owned(key.to_string()), Cow::Owned(val.to_string())))
+        let inner = self
+            .iter()
+            .filter_map(|(key, val)| {
+                val.get_unique().map(|value| (
+                    Cow::Owned(key.borrow().to_string()),
+                    Cow::Owned(value.to_string())
+                ))
+            })
             .collect();
 
         NormalizedParameter {
             inner,
         }
+    }
+}
+
+unsafe impl UniqueValue for str {
+    fn get_unique(&self) -> Option<&str> {
+        Some(self)
+    }
+}
+
+unsafe impl UniqueValue for String {
+    fn get_unique(&self) -> Option<&str> {
+        Some(&self)
+    }
+}
+
+unsafe impl<'a, V> UniqueValue for &'a V 
+    where V: AsRef<str> + ?Sized
+{
+    fn get_unique(&self) -> Option<&str> {
+        Some(self.as_ref())
+    }
+}
+
+unsafe impl<'a> UniqueValue for Cow<'a, str> {
+    fn get_unique(&self) -> Option<&str> {
+        Some(self.as_ref())
+    }
+}
+
+unsafe impl<V: UniqueValue> UniqueValue for [V] {
+    fn get_unique(&self) -> Option<&str> {
+        if self.len() > 1 {
+            None
+        } else {
+            self.get(0).and_then(V::get_unique)
+        }
+    }
+}
+
+unsafe impl<V: UniqueValue + ?Sized> UniqueValue for Box<V> {
+    fn get_unique(&self) -> Option<&str> {
+        (**self).get_unique()
+    }
+}
+
+unsafe impl<V: UniqueValue + ?Sized> UniqueValue for Rc<V> {
+    fn get_unique(&self) -> Option<&str> {
+        (**self).get_unique()
+    }
+}
+
+unsafe impl<V: UniqueValue + ?Sized> UniqueValue for Arc<V> {
+    fn get_unique(&self) -> Option<&str> {
+        (**self).get_unique()
+    }
+}
+
+unsafe impl<V: UniqueValue> UniqueValue for Vec<V> {
+    fn get_unique(&self) -> Option<&str> {
+        if self.len() > 1 {
+            None
+        } else {
+            self.get(0).and_then(V::get_unique)
+        }
+    }
+}
+
+mod test {
+    use super::*;
+
+    /// Compilation tests for various possible QueryParameter impls.
+    #[allow(unused)]
+    #[allow(dead_code)]
+    fn test_query_parameter_impls() {
+        let _ = (&HashMap::<String, String>::new()) as &QueryParameter;
+        let _ = (&HashMap::<&'static str, &'static str>::new()) as &QueryParameter;
+        let _ = (&HashMap::<Cow<'static, str>, Cow<'static, str>>::new()) as &QueryParameter;
+
+        let _ = (&HashMap::<String, Vec<String>>::new()) as &QueryParameter;
+        let _ = (&HashMap::<String, Box<String>>::new()) as &QueryParameter;
+        let _ = (&HashMap::<String, Box<[Cow<'static, str>]>>::new()) as &QueryParameter;
     }
 }
