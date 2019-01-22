86333504acbe8d12e7bfbb53b559c4103752a35b
Andreas Molzer <andreas.molzer@gmx.de>
2019-01-22 01:55:36 +0100

    Iron example client and endpoint structure

--- examples/iron.rs
+++ examples/iron.rs
@@ -3,9 +3,71 @@ extern crate oxide_auth;
 extern crate router;
 
 use std::sync::{Arc, Mutex};
+use std::thread::spawn;
+
+use iron::{Iron, Request, Response};
+use iron::middleware::Handler;
+
+use oxide_auth::frontends::simple::endpoint::Generic;
+use oxide_auth::primitives::prelude::*;
 
 #[path = "support/iron.rs"]
 mod support;
 
+struct EndpointState {
+    registrar: Mutex<ClientMap>,
+    authorizer: Mutex<AuthMap<RandomGenerator>>,
+    issuer: Mutex<TokenSigner>,
+}
+
+fn main_router() -> impl Handler + 'static {
+    let state = Arc::new(EndpointState::preconfigured());
+    let mut router = router::Router::new();
+    router.get("/authorize", |request: &mut Request| {
+        Ok(Response::new())
+    }, "authorization_get");
+    router.post("/authorize", |request: &mut Request| {
+        Ok(Response::new())
+    }, "authorization_post");
+    router.get("/token", |request: &mut Request| {
+        Ok(Response::new())
+    }, "token");
+    router.get("/", |request: &mut Request| {
+        Ok(Response::new())
+    }, "protected");
+
+    router
+}
+
 fn main() {
+    let server = spawn(|| {
+        Iron::new(main_router())
+            .http("127.0.0.1:8020")
+            .expect("Failed to launch authorization server");
+    });
+
+    let client = spawn(|| {
+        Iron::new(support::dummy_client())
+            .http("127.0.0.1:8021")
+            .expect("Failed to launch client");
+    });
+
+    support::open_in_browser();
+
+    server.join().unwrap();
+    client.join().unwrap();
+}
+
+impl EndpointState {
+    fn preconfigured() -> Self {
+        EndpointState {
+            registrar: Mutex::new(vec![
+                Client::public("LocalClient",
+                    "http://localhost:8021/endpoint".parse().unwrap(),
+                    "default-scope".parse().unwrap())
+            ].into_iter().collect()),
+            authorizer: Mutex::new(AuthMap::new(RandomGenerator::new(16))),
+            issuer: Mutex::new(TokenSigner::ephemeral()),
+        }
+    }
 }
--- examples/support/iron.rs
+++ examples/support/iron.rs
@@ -1,4 +1,3 @@
-extern crate iron;
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
@@ -10,22 +9,46 @@ pub use self::generic::*;
 
 use std::collections::HashMap;
 use std::io::Read;
-use self::iron::prelude::*;
-use self::iron::{headers, modifiers, status};
-use self::reqwest::header::Authorization;
+use std::sync::{Arc, RwLock};
+
+use iron::{headers, modifiers, IronResult, Request, Response};
+use iron::middleware::Handler;
+use iron::status::Status;
+use self::reqwest::header;
 
 /// Rough client function mirroring core functionality of an oauth client. This is not actually
 /// needed in your implementation but merely exists to provide an interactive example. It will
 /// always identify itself as `LocalClient` with redirect url `http://localhost:8021/endpoint`.
-pub fn dummy_client(req: &mut Request) -> IronResult<Response> {
+
+#[derive(Default)]
+struct State {
+    token: RwLock<Option<String>>,
+    token_map: RwLock<Option<String>>,
+}
+
+pub fn dummy_client() -> impl Handler + 'static {
+    let get_state = Arc::new(State::default());
+    let endpoint_state = get_state.clone();
+    let mut router = router::Router::new();
+    router.get("/endpoint", move |request: &mut Request| endpoint(get_state.clone(), request), "endpoint");
+    router.get("/", move |request: &mut Request| view(endpoint_state.clone(), request), "view");
+    router
+}
+
+/// Receive the authorization codes at 'http://localhost:8021/endpoint'.
+fn endpoint(state: Arc<State>, req: &mut Request) -> IronResult<Response> {
     // Check the received parameters in the input
-    let query = req.url.as_ref().query_pairs().collect::<HashMap<_, _>>();
+    let query = req.url.as_ref()
+        .query_pairs()
+        .collect::<HashMap<_, _>>();
+
     if let Some(error) = query.get("error") {
-        let message = "Error during owner authorization: ".to_string() + error.as_ref();
-        return Ok(Response::with((status::Ok, message)));
+        let message = format!("Error during owner authorization: {}", error.as_ref());
+        return Ok(Response::with((Status::Ok, message)));
     };
+
     let code = match query.get("code") {
-        None => return Ok(Response::with((status::BadRequest, "Missing code"))),
+        None => return Ok(Response::with((Status::BadRequest, "Missing code"))),
         Some(v) => v.clone()
     };
 
@@ -39,21 +62,60 @@ pub fn dummy_client(req: &mut Request) -> IronResult<Response> {
     let access_token_request = client
         .post("http://localhost:8020/token")
         .form(&params).build().unwrap();
-    let mut token_response = client.execute(access_token_request).unwrap();
+    let mut token_response = match client.execute(access_token_request) {
+        Ok(response) => response,
+        Err(_) => return Ok(Response::with((Status::InternalServerError, "Could not fetch bearer token"))),
+    };
+
     let mut token = String::new();
     token_response.read_to_string(&mut token).unwrap();
     let token_map: HashMap<String, String> = serde_json::from_str(&token).unwrap();
 
-    if token_map.get("error").is_some() || !token_map.get("access_token").is_some() {
-        return Ok(Response::with((status::BadRequest, token)));
+    if token_map.get("error").is_some() {
+        return Ok(Response::with((Status::BadRequest, token)));
     }
 
+    let token = match token_map.get("access_token") {
+        None => return Ok(Response::with((Status::BadRequest, token))),
+        Some(token) => token,
+    };
+
+    let token_map = serde_json::to_string_pretty(&token_map).unwrap();
+    let token_map = token_map.replace(",", ",</br>");
+
+    let mut set_map = state.token_map.write().unwrap();
+    *set_map = Some(token_map);
+
+    let mut set_token = state.token.write().unwrap();
+    *set_token = Some(token.to_string());
+
+    let mut response = Response::with(Status::Ok);
+    response.headers.set(headers::Location("/".into()));
+    Ok(response)
+}
+
+fn view(state: Arc<State>, req: &mut Request) -> IronResult<Response> {
+    let token = state.token.read().unwrap();
+    let token = match *token {
+        None => return Ok(Response::with((Status::Ok, "No token granted yet"))),
+        Some(ref token) => token,
+    };
+
+    let token_map = state.token_map.read().unwrap();
+    let token_map = token_map.as_ref().unwrap();
+
+    let client = reqwest::Client::new();
     // Request the page with the oauth token
     let page_request = client
         .get("http://localhost:8020/")
-        .header(Authorization("Bearer ".to_string() + token_map.get("access_token").unwrap()))
+        .header(header::AUTHORIZATION, format!("Bearer {}", token))
         .build().unwrap();
-    let mut page_response = client.execute(page_request).unwrap();
+
+    let mut page_response = match client.execute(page_request) {
+        Ok(response) => response,
+        Err(_) => return Ok(Response::with((Status::BadRequest, "Could not access protected resource"))),
+    };
+
     let mut protected_page = String::new();
     page_response.read_to_string(&mut protected_page).unwrap();
 
@@ -73,7 +135,7 @@ pub fn dummy_client(req: &mut Request) -> IronResult<Response> {
         </main></html>", token, protected_page);
 
     Ok(Response::with((
-        status::Ok,
+        Status::Ok,
         modifiers::Header(headers::ContentType::html()),
         display_page,
     )))
