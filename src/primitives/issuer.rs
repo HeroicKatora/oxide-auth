6bb42d65ee37ec64b94d5b4aa4122967501407bc
Andreas Molzer <andreas.molzer@gmx.de>
2019-08-09 15:25:01 +0200

    Fixup issuer tests and doctests

--- src/primitives/issuer.rs
+++ src/primitives/issuer.rs
@@ -178,9 +178,16 @@ impl IssuedToken {
     ///
     /// struct MyIssuer;
     ///
+    /// impl MyIssuer {
+    ///     fn access_token(&mut self, grant: &Grant) -> String {
+    ///         // .. your implementation
+    /// #       unimplemented!()
+    ///     }
+    /// }
+    ///
     /// impl Issuer for MyIssuer {
     ///     fn issue(&mut self, mut grant: Grant) -> Result<IssuedToken, ()> {
-    ///         let access = self.access_token(&grant);
+    ///         let token = self.access_token(&grant);
     ///         Ok(IssuedToken::without_refresh(token, grant.until))
     ///     }
     ///     // …
@@ -517,6 +524,17 @@ pub mod tests {
     use primitives::generator::RandomGenerator;
     use chrono::{Duration, Utc};
 
+    fn grant_template() -> Grant {
+        Grant {
+            client_id: "Client".to_string(),
+            owner_id: "Owner".to_string(),
+            redirect_uri: "https://example.com".parse().unwrap(),
+            scope: "default".parse().unwrap(),
+            until: Utc::now() + Duration::hours(1),
+            extensions: Extensions::new(),
+        }
+    }
+
     /// Tests the simplest invariants that should be upheld by all authorizers.
     ///
     /// This create a token, without any extensions, an lets the issuer generate a issued token.
@@ -525,14 +543,7 @@ pub mod tests {
     ///
     /// Custom implementations may want to import and use this in their own tests.
     pub fn simple_test_suite(issuer: &mut dyn Issuer) {
-        let request = Grant {
-            client_id: "Client".to_string(),
-            owner_id: "Owner".to_string(),
-            redirect_uri: "https://example.com".parse().unwrap(),
-            scope: "default".parse().unwrap(),
-            until: Utc::now() + Duration::hours(1),
-            extensions: Extensions::new(),
-        };
+        let request = grant_template();
 
         let issued = issuer.issue(request.clone())
             .expect("Issuing failed");
@@ -556,13 +567,34 @@ pub mod tests {
     #[test]
     fn signer_test_suite() {
         let mut signer = TokenSigner::ephemeral();
+        // Refresh tokens must be unique if generated. If they are not even generated, they are
+        // obviously not unique.
+        signer.generate_refresh_tokens(true);
         simple_test_suite(&mut signer);
+    }
+
+    #[test]
+    fn signer_no_default_refresh() {
+        let mut signer = TokenSigner::ephemeral();
+        let issued = signer.issue(grant_template());
+
+        let token = issued.expect("Issuing without refresh token failed");
+        assert!(!token.refreshable());
     }
 
     #[test]
     fn random_test_suite() {
         let mut token_map = TokenMap::new(RandomGenerator::new(16));
         simple_test_suite(&mut token_map);
+    }
+
+    #[test]
+    fn random_has_refresh() {
+        let mut token_map = TokenMap::new(RandomGenerator::new(16));
+        let issued = token_map.issue(grant_template());
+
+        let token = issued.expect("Issuing without refresh token failed");
+        assert!(token.refreshable());
     }
 
     #[test]
