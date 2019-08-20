9e8ebdee476822a56a2096cf932782544a9205fc
Andreas Molzer <andreas.molzer@gmx.de>
2019-08-20 20:51:24 +0200

    Rework rouille into wrapper types
    The crate no longer declares the trait, nor does it declare the request
    or response types. Hence, we need to make new-type wrappers instead. To
    improve ergonomics there should likely be a trait to wrap requests with
    methods, which could also precalculate and thus optimize the internal
    representation if desired (measure this).

--- oxide-auth-rouille/rouille.rs
+++ oxide-auth-rouille/rouille.rs
@@ -2,19 +2,16 @@
 //!
 //! Following the simplistic and minimal style of rouille, this module defines only the
 //! implementations for `WebRequest` and `WebResponse` and re-exports the available flows.
-extern crate rouille;
-extern crate serde_urlencoded;
-
 use std::borrow::Cow;
 
-use endpoint::{QueryParameter, WebRequest, WebResponse};
+use oxide_auth::endpoint::{QueryParameter, WebRequest, WebResponse};
 
-use self::rouille::{Request, Response, ResponseBody};
+use rouille;
 use url::Url;
 
 // In the spirit of simplicity, this module does not implement any wrapper structures.  In order to
 // allow efficient and intuitive usage, we simply re-export common structures.
-pub use frontends::simple::endpoint::{FnSolicitor, Generic as GenericEndpoint, Vacant};
+pub use oxide_auth::frontends::simple::endpoint::{FnSolicitor, Generic as GenericEndpoint, Vacant};
 
 /// Something went wrong with the rouille http request or response.
 #[derive(Debug)]
@@ -26,31 +23,45 @@ pub enum WebError {
     Encoding,
 }
 
-impl<'a> WebRequest for &'a Request {
+pub struct Request<'a> {
+    inner: &'a rouille::Request,
+}
+
+pub struct Response {
+    inner: rouille::Response,
+}
+
+impl<'a> Request<'a> {
+    pub fn new(inner: &'a rouille::Request) -> Self {
+        Request { inner }
+    }
+}
+
+impl<'a> WebRequest for Request<'a> {
     type Error = WebError;
     type Response = Response;
 
     fn query(&mut self) -> Result<Cow<dyn QueryParameter + 'static>, Self::Error> {
-        let query = self.raw_query_string();
+        let query = self.inner.raw_query_string();
         let data = serde_urlencoded::from_str(query)
             .map_err(|_| WebError::Encoding)?;
         Ok(Cow::Owned(data))
     }
 
     fn urlbody(&mut self) -> Result<Cow<dyn QueryParameter + 'static>, Self::Error> {
-        match self.header("Content-Type") {
+        match self.inner.header("Content-Type") {
             None | Some("application/x-www-form-urlencoded") => (),
             _ => return Err(WebError::Encoding),
         }
 
-        let body = self.data().ok_or(WebError::Encoding)?;
+        let body = self.inner.data().ok_or(WebError::Encoding)?;
         let data = serde_urlencoded::from_reader(body)
             .map_err(|_| WebError::Encoding)?;
         Ok(Cow::Owned(data))
     }
 
     fn authheader(&mut self) -> Result<Option<Cow<str>>, Self::Error> {
-        Ok(self.header("Authorization").map(|st| st.into()))
+        Ok(self.inner.header("Authorization").map(|st| st.into()))
     }
 }
 
@@ -58,40 +69,40 @@ impl WebResponse for Response {
     type Error = WebError;
 
     fn ok(&mut self) -> Result<(), Self::Error> {
-        self.status_code = 200;
+        self.inner.status_code = 200;
         Ok(())
     }
 
     fn redirect(&mut self, url: Url) -> Result<(), Self::Error> {
-        self.status_code = 302;
-        self.headers.retain(|header| !header.0.eq_ignore_ascii_case("Location"));
-        self.headers.push(("Location".into(), url.into_string().into()));
+        self.inner.status_code = 302;
+        self.inner.headers.retain(|header| !header.0.eq_ignore_ascii_case("Location"));
+        self.inner.headers.push(("Location".into(), url.into_string().into()));
         Ok(())
     }
 
     fn client_error(&mut self) -> Result<(), Self::Error> {
-        self.status_code = 400;
+        self.inner.status_code = 400;
         Ok(())
     }
 
     fn unauthorized(&mut self, kind: &str) -> Result<(), Self::Error> {
-        self.status_code = 401;
-        self.headers.retain(|header| !header.0.eq_ignore_ascii_case("www-authenticate"));
-        self.headers.push(("WWW-Authenticate".into(), kind.to_string().into()));
+        self.inner.status_code = 401;
+        self.inner.headers.retain(|header| !header.0.eq_ignore_ascii_case("www-authenticate"));
+        self.inner.headers.push(("WWW-Authenticate".into(), kind.to_string().into()));
         Ok(())
     }
 
     fn body_text(&mut self, text: &str) -> Result<(), Self::Error> {
-        self.headers.retain(|header| !header.0.eq_ignore_ascii_case("Content-Type"));
-        self.headers.push(("Content-Type".into(), "text/plain".into()));
-        self.data = ResponseBody::from_string(text);
+        self.inner.headers.retain(|header| !header.0.eq_ignore_ascii_case("Content-Type"));
+        self.inner.headers.push(("Content-Type".into(), "text/plain".into()));
+        self.inner.data = rouille::ResponseBody::from_string(text);
         Ok(())
     }
 
     fn body_json(&mut self, data: &str) -> Result<(), Self::Error> {
-        self.headers.retain(|header| !header.0.eq_ignore_ascii_case("Content-Type"));
-        self.headers.push(("Content-Type".into(), "application/json".into()));
-        self.data = ResponseBody::from_string(data);
+        self.inner.headers.retain(|header| !header.0.eq_ignore_ascii_case("Content-Type"));
+        self.inner.headers.push(("Content-Type".into(), "application/json".into()));
+        self.inner.data = rouille::ResponseBody::from_string(data);
         Ok(())
     }
 }
@@ -102,7 +113,8 @@ mod tests {
     
     #[test]
     fn multi_query() {
-        let mut request = &Request::fake_http("GET", "/authorize?fine=val&param=a&param=b", vec![], vec![]);
+        let request = &rouille::Request::fake_http("GET", "/authorize?fine=val&param=a&param=b", vec![], vec![]);
+        let mut request = Request::new(request);
         let query = WebRequest::query(&mut request).unwrap();
 
         assert_eq!(Some(Cow::Borrowed("val")), query.unique_value("fine"));
