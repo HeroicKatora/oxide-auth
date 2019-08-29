b24937df8f9d39e6a2468668505b7ffa8ae8c288
asonix <asonix@asonix.dog>
2019-08-28 19:50:12 -0500

    Port in oxide-auth-iron from asonix/subcrates

--- oxide-auth-iron/src/lib.rs
+++ oxide-auth-iron/src/lib.rs
@@ -3,11 +3,14 @@
 //! ## Hello world
 //!
 extern crate iron;
+extern crate oxide_auth;
+extern crate serde_urlencoded;
+extern crate url;
 
 use std::borrow::Cow;
 
-use endpoint::{OAuthError, QueryParameter, WebRequest, WebResponse};
-use frontends::simple::endpoint::Error as SimpleError;
+use oxide_auth::endpoint::{OAuthError as EndpointError, QueryParameter, WebRequest, WebResponse};
+use oxide_auth::frontends::simple::endpoint::Error as SimpleError;
 
 use self::iron::{Request, Response};
 use self::iron::error::IronError;
@@ -21,94 +24,150 @@ pub enum Error {
     BadRequest,
 }
 
+#[derive(Debug)]
+pub struct OAuthRequest<'a, 'b, 'c: 'b>(pub &'a mut Request<'b, 'c>);
+
+impl<'a, 'b, 'c: 'b> OAuthRequest<'a, 'b, 'c> {
+    pub fn from_request(request: &'a mut Request<'b, 'c>) -> Self {
+        OAuthRequest(request)
+    }
+}
+
+impl<'a, 'b, 'c: 'b> From<&'a mut Request<'b, 'c>> for OAuthRequest<'a, 'b, 'c> {
+    fn from(r: &'a mut Request<'b, 'c>) -> Self {
+        OAuthRequest::from_request(r)
+    }
+}
+
 /// Requests are handed as mutable reference to the underlying object.
-impl<'a, 'b, 'c: 'b> WebRequest for &'a mut Request<'b, 'c> {
-    type Response = Response;
+impl<'a, 'b, 'c: 'b> WebRequest for OAuthRequest<'a, 'b, 'c> {
+    type Response = OAuthResponse;
     type Error = Error;
 
     fn query(&mut self) -> Result<Cow<dyn QueryParameter + 'static>, Self::Error> {
-        serde_urlencoded::from_str(self.url.query().unwrap_or(""))
+        serde_urlencoded::from_str(self.0.url.query().unwrap_or(""))
             .map_err(|_| Error::BadRequest)
             .map(Cow::Owned)
     }
 
     fn urlbody(&mut self) -> Result<Cow<dyn QueryParameter + 'static>, Self::Error> {
-        let content_type = self.headers.get::<headers::ContentType>();
+        let content_type = self.0.headers.get::<headers::ContentType>();
         let formatted = content_type
             .map(|ct| ct == &headers::ContentType::form_url_encoded())
             .unwrap_or(false);
         if !formatted {
-            return Err(Error::BadRequest)
+            return Err(Error::BadRequest);
         }
 
-        serde_urlencoded::from_reader(&mut self.body)
+        serde_urlencoded::from_reader(&mut self.0.body)
             .map_err(|_| Error::BadRequest)
             .map(Cow::Owned)
     }
 
     fn authheader(&mut self) -> Result<Option<Cow<str>>, Self::Error> {
         // Get the raw header.
-        match self.headers.get::<headers::Authorization<String>>() {
+        match self.0.headers.get::<headers::Authorization<String>>() {
             None => Ok(None),
             Some(header) => Ok(Some(Cow::Borrowed(&header.0))),
         }
     }
 }
 
-impl WebResponse for Response {
+impl WebResponse for OAuthResponse {
     type Error = Error;
 
-    fn ok(&mut self) -> Result<(), Self::Error> { 
-        self.status = Some(Status::Ok);
+    fn ok(&mut self) -> Result<(), Self::Error> {
+        self.0.status = Some(Status::Ok);
         Ok(())
     }
 
-    fn redirect(&mut self, url: Url) -> Result<(), Self::Error> { 
-        self.status = Some(Status::Found);
-        self.headers.set(headers::Location(url.into_string()));
+    fn redirect(&mut self, url: Url) -> Result<(), Self::Error> {
+        self.0.status = Some(Status::Found);
+        self.0.headers.set(headers::Location(url.into_string()));
         Ok(())
     }
 
     fn client_error(&mut self) -> Result<(), Self::Error> {
-        self.status = Some(Status::BadRequest);
+        self.0.status = Some(Status::BadRequest);
         Ok(())
     }
 
-    fn unauthorized(&mut self, header_value: &str) -> Result<(), Self::Error> { 
-        self.status = Some(Status::Unauthorized);
+    fn unauthorized(&mut self, header_value: &str) -> Result<(), Self::Error> {
+        self.0.status = Some(Status::Unauthorized);
         let value_owned = header_value.as_bytes().to_vec();
-        self.headers.set_raw("WWW-Authenticate", vec![value_owned]);
+        self.0
+            .headers
+            .set_raw("WWW-Authenticate", vec![value_owned]);
         Ok(())
     }
 
-    fn body_text(&mut self, text: &str) -> Result<(), Self::Error> { 
-        self.headers.set(headers::ContentType::plaintext());
-        self.body = Some(Box::new(text.to_string()));
+    fn body_text(&mut self, text: &str) -> Result<(), Self::Error> {
+        self.0.headers.set(headers::ContentType::plaintext());
+        self.0.body = Some(Box::new(text.to_string()));
         Ok(())
     }
 
-    fn body_json(&mut self, data: &str) -> Result<(), Self::Error> { 
-        self.headers.set(headers::ContentType::json());
-        self.body = Some(Box::new(data.to_string()));
+    fn body_json(&mut self, data: &str) -> Result<(), Self::Error> {
+        self.0.headers.set(headers::ContentType::json());
+        self.0.body = Some(Box::new(data.to_string()));
         Ok(())
     }
 }
 
-// impl ErrorTrait for OAuthError { }
-
-impl<'a, 'b, 'c: 'b> From<SimpleError<&'a mut Request<'b, 'c>>> for IronError {
-    fn from(error: SimpleError<&'a mut Request<'b, 'c>>) -> Self {
+impl<'a, 'b, 'c: 'b> From<SimpleError<OAuthRequest<'a, 'b, 'c>>> for OAuthError {
+    fn from(error: SimpleError<OAuthRequest<'a, 'b, 'c>>) -> Self {
         let as_oauth = match error {
-            SimpleError::Web(Error::BadRequest) => OAuthError::BadRequest,
+            SimpleError::Web(Error::BadRequest) => EndpointError::BadRequest,
             SimpleError::OAuth(oauth) => oauth,
         };
-        
+
         let status = match as_oauth {
-            OAuthError::BadRequest => Status::BadRequest,
-            OAuthError::DenySilently => Status::BadRequest,
-            OAuthError::PrimitiveError => Status::InternalServerError,
+            EndpointError::BadRequest => Status::BadRequest,
+            EndpointError::DenySilently => Status::BadRequest,
+            EndpointError::PrimitiveError => Status::InternalServerError,
         };
 
-        IronError::new(as_oauth, status)
+        OAuthError(IronError::new(as_oauth, status))
     }
 }
+
+#[derive(Debug)]
+pub struct OAuthResponse(pub Response);
+
+impl OAuthResponse {
+    pub fn new() -> Self {
+        OAuthResponse(Response::new())
+    }
+
+    pub fn from_response(response: Response) -> Self {
+        OAuthResponse(response)
+    }
+}
+
+impl From<Response> for OAuthResponse {
+    fn from(r: Response) -> Self {
+        OAuthResponse::from_response(r)
+    }
+}
+
+impl Into<Response> for OAuthResponse {
+    fn into(self) -> Response {
+        self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct OAuthError(pub IronError);
+
+impl From<IronError> for OAuthError {
+    fn from(e: IronError) -> Self {
+        OAuthError(e)
+    }
+}
+
+impl Into<IronError> for OAuthError {
+    fn into(self) -> IronError {
+        self.0
+    }
+}
+
