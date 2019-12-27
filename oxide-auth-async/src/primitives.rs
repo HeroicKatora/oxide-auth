8dcb4a5d8a78672911a67036ada491b1d75deb74
HeroicKatora <andreas.molzer@gmx.de>
2019-12-27 13:32:09 +0100

    Straight forward port of Autorizer interface

--- oxide-auth-async/src/primitives.rs
+++ oxide-auth-async/src/primitives.rs
@@ -1,8 +1,28 @@
 //! Async versions of all primitives traits.
 use async_trait::async_trait;
-use oxide_auth::primitives::{grant::Grant, scope::Scope};
+use oxide_auth::primitives::{authorizer, issuer, registrar, grant::Grant, scope::Scope};
 use oxide_auth::primitives::issuer::{IssuedToken, RefreshedToken};
 use oxide_auth::primitives::registrar::{ClientUrl, BoundClient, RegistrarError, PreGrant};
+
+#[async_trait(?Send)]
+pub trait Authorizer {
+    async fn authorize(&mut self, _: Grant) -> Result<String, ()>;
+
+    async fn extract(&mut self, _: &str) -> Result<Option<Grant>, ()>;
+}
+
+#[async_trait(?Send)]
+impl<T> Authorizer for T
+    where T: authorizer::Authorizer + ?Sized,
+{
+    async fn authorize(&mut self, grant: Grant) -> Result<String, ()> {
+        authorizer::Authorizer::authorize(self, grant)
+    }
+
+    async fn extract(&mut self, token: &str) -> Result<Option<Grant>, ()> {
+        authorizer::Authorizer::extract(self, token)
+    }
+}
 
 #[async_trait(?Send)]
 pub trait Issuer {
@@ -17,22 +37,22 @@ pub trait Issuer {
 
 #[async_trait(?Send)]
 impl<T> Issuer for T
-    where T: oxide_auth::primitives::issuer::Issuer + ?Sized,
+    where T: issuer::Issuer + ?Sized,
 {
     async fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
-        oxide_auth::primitives::issuer::Issuer::issue(self, grant)
+        issuer::Issuer::issue(self, grant)
     }
 
     async fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, ()> {
-        oxide_auth::primitives::issuer::Issuer::refresh(self, token, grant)
+        issuer::Issuer::refresh(self, token, grant)
     }
 
     async fn recover_token(&mut self, token: &str) -> Result<Option<Grant>, ()> {
-        oxide_auth::primitives::issuer::Issuer::recover_token(self, token)
+        issuer::Issuer::recover_token(self, token)
     }
 
     async fn recover_refresh(&mut self, token: &str) -> Result<Option<Grant>, ()> {
-        oxide_auth::primitives::issuer::Issuer::recover_refresh(self, token)
+        issuer::Issuer::recover_refresh(self, token)
     }
 }
 
@@ -50,23 +70,23 @@ pub trait Registrar {
 
 #[async_trait(?Send)]
 impl<T> Registrar for T
-    where T: oxide_auth::primitives::registrar::Registrar + ?Sized,
+    where T: registrar::Registrar + ?Sized,
 {
     async fn bound_redirect<'a>(&self, bound: ClientUrl<'a>)
         -> Result<BoundClient<'a>, RegistrarError>
     {
-        oxide_auth::primitives::registrar::Registrar::bound_redirect(self, bound)
+        registrar::Registrar::bound_redirect(self, bound)
     }
 
     async fn negotiate<'a>(&self, client: BoundClient<'a>, scope: Option<Scope>)
         -> Result<PreGrant, RegistrarError>
     {
-        oxide_auth::primitives::registrar::Registrar::negotiate(self, client, scope)
+        registrar::Registrar::negotiate(self, client, scope)
     }
 
     async fn check(&self, client_id: &str, passphrase: Option<&[u8]>)
         -> Result<(), RegistrarError>
     {
-        oxide_auth::primitives::registrar::Registrar::check(self, client_id, passphrase)
+        registrar::Registrar::check(self, client_id, passphrase)
     }
 }
