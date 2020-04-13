c237a1a3de1136bd75648f94198e5c2f3ef355a5
Andreas Molzer <andreas.molzer@gmx.de>
2020-04-13 17:38:20 +0200

    An async implementation of resource

--- oxide-auth-async/src/code_grant.rs
+++ oxide-auth-async/src/code_grant.rs
@@ -51,3 +51,52 @@ pub mod refresh {
         }
     }
 }
+
+pub mod resource {
+    use oxide_auth::code_grant::resource::{Error, Input, Output, Resource, Request};
+    use oxide_auth::primitives::grant::Grant;
+    use oxide_auth::primitives::scope::Scope;
+
+    pub trait Endpoint {
+        /// The list of possible scopes required by the resource endpoint.
+        fn scopes(&mut self) -> &[Scope];
+
+        /// Recover and test the provided refresh token then issue new tokens.
+        fn issuer(&mut self) -> &mut dyn crate::primitives::Issuer;
+    }
+
+    pub async fn protect(handler: &mut dyn Endpoint, req: &dyn Request) -> Result<Grant, Error> {
+        enum Requested {
+            None,
+            Request,
+            Scopes,
+            Grant(String),
+        }
+
+        let mut resource = Resource::new();
+        let mut requested = Requested::None;
+        loop {
+            let input = match requested {
+                Requested::None => Input::None,
+                Requested::Request => Input::Request { request: req },
+                Requested::Scopes => Input::Scopes(handler.scopes()),
+                Requested::Grant(token) => {
+                    let grant = handler
+                        .issuer()
+                        .recover_token(&token)
+                        .await
+                        .map_err(|_| Error::PrimitiveError)?;
+                    Input::Recovered(grant)
+                }
+            };
+
+            requested = match resource.advance(input) {
+                Output::Err(error) => return Err(error),
+                Output::Ok(grant) => return Ok(grant),
+                Output::GetRequest => Requested::Request,
+                Output::DetermineScopes => Requested::Scopes,
+                Output::Recover { token } => Requested::Grant(token.to_string()),
+            };
+        }
+    }
+}
